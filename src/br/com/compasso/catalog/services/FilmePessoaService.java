6d9d88bf7e014117ea5463a14b496f5b81a06700
Augusto <augustoschweickardt@gmail.com>
2020-12-15 16:58:01 -0300

    Fazendo métodos removeById e adiciona em FilmePessoaService

--- src/br/com/compasso/catalog/services/FilmePessoaService.java
+++ src/br/com/compasso/catalog/services/FilmePessoaService.java
@@ -8,21 +8,34 @@ import br.com.compasso.catalog.models.FilmePessoa;
 import br.com.compasso.catalog.models.Pessoa;
 
 public class FilmePessoaService {
-	
+
 	private List<FilmePessoa> fp = new ArrayList<>();
-	
-	//Métodos
-	
-	public List<FilmePessoa> imprimeById (int idPessoa) {
+
+	// Métodos
+
+	public List<FilmePessoa> imprimeById(int idPessoa) {
 		return fp;
 	}
-	
-	public void removeById (int idPessoa, int idFilme){
+
+	public boolean removeById(int idPessoa, int idFilme) {
 		
-	}
-	
-	public void adiciona (Pessoa pessoa, Filme filme){
+		for (FilmePessoa filmePessoa : fp) {
+			if(filmePessoa.getPessoa().getId() == idPessoa && filmePessoa.getFilme().getId() == idFilme) {
+				fp.remove(filmePessoa);
+				System.out.println("Filme removido com sucesso!");
+				return true;
+			}
+		}
 		
+		System.out.println("O filme não estava em seu catálogo!");
+		return false;
 	}
-	
+
+	public void adiciona(Pessoa pessoa, Filme filme) {
+		
+		FilmePessoa filmepessoa = new FilmePessoa(filme, pessoa);
+		
+		fp.add(filmepessoa);
+	}
+
 }
