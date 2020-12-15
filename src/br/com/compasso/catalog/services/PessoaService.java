e511b9f13b8fd962d5cd9c927ddde27f42399c9f
guilhermecalderaro <guilhermecald96@gmail.com>
2020-12-15 18:15:22 -0300

    Implementado os métodos da classe PessoaService

--- src/br/com/compasso/catalog/services/PessoaService.java
+++ src/br/com/compasso/catalog/services/PessoaService.java
@@ -3,29 +3,59 @@ package br.com.compasso.catalog.services;
 import java.util.ArrayList;
 import java.util.List;
 
-import br.com.compasso.catalog.models.Filme;
 import br.com.compasso.catalog.models.Pessoa;
 
 public class PessoaService {
 
 	private List<Pessoa> pessoas = new ArrayList<>();
+	private int id = 0;
 
 	//Métodos
 
-	public List<Pessoa> imprimeByName(String pessoa) {
-		return pessoas;
-	}
-
-	public void removeById(int idPessoa, String nomePessoa) {
-
-	}
-
-	public void adiciona(String pessoa) {
+	public void removeById(int idPessoa) {
 		
+		Pessoa pessoaRemovida = null; 
+		
+		for (Pessoa pessoa : pessoas) {
+			if(pessoa.getId() == idPessoa) {
+				pessoaRemovida = pessoa;
+			}			
+		}
+		
+		if(pessoaRemovida != null) {
+			pessoas.remove(pessoaRemovida);
+			System.out.println("Pessoa removida com sucesso!");
+		} else {
+			System.out.println("Não foi encotrada nenhuma pessoa com esse id!");
+		}
+
+	}
+
+	public void adiciona(String nome, int idade) {
+		Pessoa novaPessoa = new Pessoa(id, nome, idade);
+		if(pessoas.contains(novaPessoa)) {
+			System.out.println("Pessoa já existe");
+		} else {
+			id++;
+			pessoas.add(novaPessoa);
+			System.out.println("Pessoa adicionada com sucesso");
+		}
 	}
 	
 	public List<Pessoa> imprimeTodos (){
 		return pessoas;
+	}
+	
+	public List<Pessoa> imprimeByName(String nome) {
+		List<Pessoa> pessoasEncontradas = new ArrayList<>();
+		
+		for (Pessoa pessoa : pessoas) {
+			if(pessoa.getNome() == nome) {
+				pessoasEncontradas.add(pessoa);
+			}
+		}
+		
+		return pessoasEncontradas;
 	}
 
 }
